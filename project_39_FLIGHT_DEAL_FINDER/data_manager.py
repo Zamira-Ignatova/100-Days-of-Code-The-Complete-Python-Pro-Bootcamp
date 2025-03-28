d90082c6b80247a84c1783486147037bc9ba0016
Zamira Ignatova <zamiraignatova@gmail.com>
2025-03-28 21:02:43 +0300

    Update data_manager.py

--- project_39_FLIGHT_DEAL_FINDER/data_manager.py
+++ project_39_FLIGHT_DEAL_FINDER/data_manager.py
@@ -8,28 +8,40 @@ dotenv.load_dotenv(dotenv_path="/Users/zamiraignatova/PycharmProjects/day_39_/.v
 class DataManager:
     def __init__(self):
         self.destination_data = {}
+        self.customer_data = {}
         self.HOST_DOMAIN = os.environ["HOST_DOMAIN"]
         self.USERNAME = os.environ["USERNAME"]
         self.AUTH_TOKEN = os.environ["AUTH_TOKEN"]
         self.PROJECT_NAME = os.environ["PROJECT_NAME"]
-        self.SHEET_NAME = os.environ["SHEET_NAME"]
+        self.SHEET_NAME_PRICES = os.environ["SHEET_NAME_PRICES"]
+        self.SHEET_NAME_USERS = os.environ["SHEET_NAME_USERS"]
         self.headers = {"Authorization": os.environ["AUTH_TOKEN"]}
 
-    def get_destination_data(self):
+    def get_destination_data_from_google_sheet(self):
         """Uses the Sheety API to GET all the data in that sheet and print it out."""
-        response_get = requests.get(
-            url=f"{self.HOST_DOMAIN}/{self.USERNAME}/{self.PROJECT_NAME}/{self.SHEET_NAME}", headers=self.headers)
-        response_get.raise_for_status()
-        self.destination_data = response_get.json()['prices']
+        response = requests.get(
+            url=f"{self.HOST_DOMAIN}/{self.USERNAME}/{self.PROJECT_NAME}/{self.SHEET_NAME_PRICES}", headers=self.headers)
+        response.raise_for_status()
+        self.destination_data = response.json()['prices']
         pprint(self.destination_data)
         return self.destination_data
 
-    def update_airport_code(self):
+    def update_iata_code_in_google_sheet(self):
         """updates the Google Sheet with the IATA codes"""
         for city in self.destination_data:
             new_data = {"price": {"iataCode": city["iataCode"]}}
             response = requests.put(
-                url=f"{self.HOST_DOMAIN}/{self.USERNAME}/{self.PROJECT_NAME}/{self.SHEET_NAME}/{city['id']}",
+                url=f"{self.HOST_DOMAIN}/{self.USERNAME}/{self.PROJECT_NAME}/{self.SHEET_NAME_PRICES}/{city['id']}",
                 json=new_data, headers=self.headers)
             response.raise_for_status()
             print(response.text)
+
+    def get_customer_emails(self):
+        response = requests.get(
+            url=f"{self.HOST_DOMAIN}/{self.USERNAME}/{self.PROJECT_NAME}/{self.SHEET_NAME_USERS}",
+            headers=self.headers)
+        response.raise_for_status()
+        self.customer_data = response.json()['users']
+        pprint(self.customer_data)
+        return self.customer_data
+
