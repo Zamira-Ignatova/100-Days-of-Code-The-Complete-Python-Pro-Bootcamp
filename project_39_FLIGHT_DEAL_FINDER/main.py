c27e0d3b9ede5eccb35f36efc5bd342b8323f181
Zamira Ignatova <zamiraignatova@gmail.com>
2025-03-28 21:02:09 +0300

    Update main.py

--- project_39_FLIGHT_DEAL_FINDER/main.py
+++ project_39_FLIGHT_DEAL_FINDER/main.py
@@ -6,19 +6,21 @@ import  time
 import datetime
 import  dotenv
 import os
-dotenv.load_dotenv(dotenv_path="your own path to the .env file")
+from pprint import pprint
+
+dotenv.load_dotenv(dotenv_path="/Users/zamiraignatova/PycharmProjects/day_39_/.venv/.env")
 
 # constants
-DEPARTURE_AIRPORT_IATA = "YOUR OWN IATA CODE"
+DEPARTURE_AIRPORT_IATA = "LON"
 TO_EMAIL_ADDRESS = os.environ["EMAIL_ADDRESS"]
-
+DAYS_OF_TRIP = 7
 #variables
 ongoing_day = datetime.datetime.now()
 tomorrow = ongoing_day + datetime.timedelta(days=1)
-one_week_after_tomorrow= ongoing_day + datetime.timedelta(days=7)
+days_of_trip = ongoing_day + datetime.timedelta(days=DAYS_OF_TRIP)
 
 data_manager = DataManager()
-sheet_data = data_manager.get_destination_data()
+sheet_data = data_manager.get_destination_data_from_google_sheet()
 flight_search = FlightSearch()
 notification_manager = NotificationManager()
 
@@ -26,24 +28,46 @@ notification_manager = NotificationManager()
 for destination in sheet_data:
     for key, value in destination.items():
         if destination['iataCode'] == "":
-            destination['iataCode'] = flight_search.get_code_of_an_airport(city=destination['city'])
+            destination['iataCode'] = flight_search.get_iata_code_of_destination_airport(city=destination['city'])
             time.sleep(2)
-#print(f"The data from the Google Sheet: {sheet_data}")
-sheet_data = data_manager.destination_data
-data_manager.update_airport_code()
 
-#searching flights
+sheet_data = data_manager.destination_data
+data_manager.update_iata_code_in_google_sheet()
+
+#Retrieve your customer emails
+customer_data = data_manager.get_customer_emails()
+pprint(customer_data)
+customer_email_list = [row["whatIsYourEmail?"] for row in customer_data]
+pprint(customer_email_list)
+
+#searching for direct flights
 for destination in sheet_data:
     for key, value in destination.items():
         print(f"Finding the cheapest flight ✈️ to the {destination["city"]} ⏳")
-        flights = flight_search.get_flights(departure_iata_code=DEPARTURE_AIRPORT_IATA, arrival_iata_code=destination["iataCode"],
-                                  departure_date=tomorrow, return_date=one_week_after_tomorrow)
-        cheapest_flight = get_cheapest_flights(flights)
+        direct_flights = flight_search.get_all_flights(departure_iata_code=DEPARTURE_AIRPORT_IATA, arrival_iata_code=destination["iataCode"],
+                                                       departure_date=tomorrow, return_date=days_of_trip)
+        cheapest_flight = get_cheapest_flights(direct_flights)
+        print(f"Cheapest direct flight price to get to {destination['city']} is £{cheapest_flight.price}")
+        time.sleep(2)
+# searching for indirect flights
+        if cheapest_flight.price != "N/A":
+            print(f"No direct flights to {destination['city']} are available right now. We are looking for indirect flights ⏳")
+            indirect_flights = flight_search.get_all_flights(departure_iata_code=DEPARTURE_AIRPORT_IATA,
+                                                           arrival_iata_code=destination["iataCode"],
+                                                           departure_date=tomorrow, return_date=days_of_trip, is_direct_flight=False)
+            cheapest_flight = get_cheapest_flights(indirect_flights)
+            print(f"Cheapest indirect flight price to get to {destination['city']} is £{cheapest_flight.price}")
         if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
-            notification_manager.send_email(to_email_address=TO_EMAIL_ADDRESS,
-                                            message=f"PRICE ALERT!\n"
-                                                    f"There is a cheapest flight from the {cheapest_flight.departure_iata_code}"
-                                                    f" to the {cheapest_flight.arrival_iata_code} for ${cheapest_flight.price}\n"
-                                                    f" the departure is on {cheapest_flight.departure_date}, "
-                                                    f" the return day is on {cheapest_flight.return_date}.")
+            if cheapest_flight.layovers == 0:
+                message = (f"There is a cheapest flight from the {cheapest_flight.departure_iata_code}"
+                           f" to the {cheapest_flight.arrival_iata_code} for ${cheapest_flight.price}.\n"
+                           f" The departure is on {cheapest_flight.departure_date},"
+                           f" the return day is on {cheapest_flight.return_date}.")
+            else:
+                message = (
+                    f"There is a cheapest flight from the {cheapest_flight.departure_iata_code}"
+                    f" to the {cheapest_flight.arrival_iata_code} with {cheapest_flight.layovers} layover(s)"
+                    f" for ${cheapest_flight.price}.\n The departure is on {cheapest_flight.departure_date},"
+                    f" the return day is on {cheapest_flight.return_date}.")
+            notification_manager.send_email(email_list=customer_email_list, message_body=message)
 
